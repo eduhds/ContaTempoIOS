7339652d713b65b6760e24f00eadbf6c7ff100e7
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2026-06-01 16:33:38 -0300

    Initial Commit

---
+++ ContaTempo/ContaTempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContaTempoApp.swift
+//  ContaTempo
+//
+//  Created by Eduardo Henrique da Silva on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ContaTempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
